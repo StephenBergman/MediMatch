c28527732fc2f252ddf6a4c3b14ed12afdbbbdf7
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2026-01-28 17:06:40 -0500

    More settings page added

---
+++ app/settingsMoreSettings.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const settingsMoreSettings = () => {
+  return (
+    <View>
+      <Text>settingsMoreSettings</Text>
+    </View>
+  )
+}
+
+export default settingsMoreSettings
+
+const styles = StyleSheet.create({})
