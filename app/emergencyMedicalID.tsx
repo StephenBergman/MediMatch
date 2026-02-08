838a1a4013d6e1cf3dd9d6cb549daf55caa95ec0
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2026-02-08 17:58:56 -0500

    back button added to emergency medical ID

--- app/emergencyMedicalID.tsx
+++ app/emergencyMedicalID.tsx
@@ -1,14 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { router, Router } from 'expo-router';
 
 const emergencyMedicalID = () => {
+
   return (
     <View>
       <Text>emergencyMedicalID</Text>
+
+
+
+
+
+      <MaterialCommunityIcons
+					name="arrow-left"
+					size={34}
+					color={colors.icon}
+					style={styles.backButton}
+					onPress={() => router.back()}
+				/>
     </View>
+    
   )
 }
 
 export default emergencyMedicalID
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    backButton: {
+			marginTop: 20,
+			marginBottom: 30,
+			marginLeft: 20,
+		},
+})
