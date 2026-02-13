3c78ee5366eecf4f3228610f544cecd5bd7e936a
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2026-02-13 15:57:53 -0500

    added the actual pages

---
+++ app/onboarding1.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const onboarding1 = () => {
+  return (
+    <View>
+      <Text>onboarding1</Text>
+    </View>
+  )
+}
+
+export default onboarding1
+
+const styles = StyleSheet.create({})
